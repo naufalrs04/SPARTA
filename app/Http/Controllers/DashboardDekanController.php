03d50baa7f6fff7259a5cb9ed9dc73750e4ebabd
naufalrs04 <naufalrizkisaputra2004@gmail.com>
2024-09-29 20:08:31 +0700

    update

--- app/Http/Controllers/DashboardDekanController.php
+++ app/Http/Controllers/DashboardDekanController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardDekanController extends Controller
-{
-    public function index()
+{    public function index()
     {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        $user = Auth::user();
+
         $data = [
-            'nama' => 'Rocky Gerung Dekan',
-            'nim' => '0987654321',
+            'nama' => $user->nama,
+            'nim_nip' => $user->nim_nip,
         ];
 
-        return view ('dashboardDekan', compact('data'));
+        return view('dashboardDekan', compact('data', 'user'));
     }
 }
