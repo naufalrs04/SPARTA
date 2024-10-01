214151127a509ee29a6a344edcae0d7abdd077e0
naufalrs04 <naufalrizkisaputra2004@gmail.com>
2024-10-01 10:32:22 +0700

    first commit

--- database/seeders/mata_kuliah.php
+++ database/seeders/mata_kuliah.php
@@ -20,36 +20,50 @@ class mata_kuliah extends Seeder
                 'nama' => 'Pemrograman Berbasis Objek',
                 'sks' => 3,
                 'semester' => 3,
-                'prodi' => 'Teknik Informatika',
+                'prodi' => 'Informatika',
             ],
             [
                 'kode' => 'IF1235',
                 'nama' => 'Pemrograman Web',
                 'sks' => 3,
                 'semester' => 3,
-                'prodi' => 'Teknik Informatika',
+                'prodi' => 'Informatika',
             ],
             [
                 'kode' => 'IF1236',
                 'nama' => 'Pemrograman Mobile',
                 'sks' => 3,
                 'semester' => 3,
-                'prodi' => 'Teknik Informatika',
+                'prodi' => 'Informatika',
             ],
             [
                 'kode' => 'IF1237',
                 'nama' => 'Pemrograman Desktop',
                 'sks' => 3,
                 'semester' => 3,
-                'prodi' => 'Teknik Informatika',
+                'prodi' => 'Informatika',
             ],
             [
                 'kode' => 'IF1238',
                 'nama' => 'Pemrograman Jaringan',
                 'sks' => 3,
                 'semester' => 3,
-                'prodi' => 'Teknik Informatika',
+                'prodi' => 'Informatika',
             ],
+            [
+                'kode' => 'IF1239',
+                'nama' => 'Pemrograman Game',
+                'sks' => 3,
+                'semester' => 3,
+                'prodi' => 'Informatika',
+            ],
+            [
+                'kode' => 'IF1240',
+                'nama' => 'Pemrograman Database',
+                'sks' => 3,
+                'semester' => 3,
+                'prodi' => 'Informatika',
+            ]
         ];
 
         DB::table('mata_kuliahs')->insert($mata_kuliahs);
