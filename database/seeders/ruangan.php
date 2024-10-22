90bbbcb218d6b563e2013fb6133bfd5a8b9dd938
RahmanHanifs <128392694+RahmanHanifs@users.noreply.github.com>
2024-10-22 23:14:39 +0700

    new3

--- database/seeders/ruangan.php
+++ database/seeders/ruangan.php
@@ -19,51 +19,61 @@ class ruangan extends Seeder
                 'kode' => 'R001',
                 'nama' => 'E101',
                 'kapasitas' => '50',
+                'gedung' =>'E'
             ],
             [
                 'kode' => 'R002',
                 'nama' => 'A103',
                 'kapasitas' => '50',
+                'gedung' =>'A'
             ],
             [
                 'kode' => 'R003',
                 'nama' => 'A104',
                 'kapasitas' => '50',
+                'gedung' =>'A'
             ],
             [
                 'kode' => 'R004',
                 'nama' => 'E104',
                 'kapasitas' => '50',
+                'gedung' =>'E'
             ],
             [
                 'kode' => 'R005',
                 'nama' => 'E105',
                 'kapasitas' => '50',
+                'gedung' =>'E'
             ],
             [
                 'kode' => 'R006',
                 'nama' => 'E302',
                 'kapasitas' => '50',
+                'gedung' =>'E'
             ],
             [
                 'kode' => 'R007',
                 'nama' => 'E307',
                 'kapasitas' => '50',
+                'gedung' =>'E'
             ],
             [
                 'kode' => 'R008',
                 'nama' => 'K102',
                 'kapasitas' => '50',
+                'gedung' =>'K'
             ],
             [
                 'kode' => 'R009',
                 'nama' => 'K104',
                 'kapasitas' => '50',
+                'gedung' =>'K'
             ],
             [
                 'kode' => 'R010',
                 'nama' => 'B103',
                 'kapasitas' => '50',
+                'gedung' =>'B'
             ],
         ];
 
