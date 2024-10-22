b489e5b4f3cf4a3915736d1d3a6d3e26633ec082
Muhammad Luthfan L <upan.azuardi@gmail.com>
2024-10-23 01:52:07 +0700

    test

--- database/seeders/irs.php
+++ database/seeders/irs.php
@@ -15,6 +15,7 @@ class irs extends Seeder
     {
         //
         $irs = [
+            // Jadwal aman (tidak ada tabrakan ruangan)
             [
                 'mahasiswa_id' => 1,
                 'mata_kuliah_id' => 1,
@@ -45,7 +46,148 @@ class irs extends Seeder
                 'mata_kuliah_id' => 6,
                 'ruangan_id' => 6,
             ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 7,
+                'ruangan_id' => 7,
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 8,
+                'ruangan_id' => 8,
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 9,
+                'ruangan_id' => 9,
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 10,
+                'ruangan_id' => 10,
+            ],
+        
+            // Jadwal tabrakan (ruangan yang sama untuk mata kuliah yang berbeda)
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 11,
+                'ruangan_id' => 1, // Tabrakan dengan mata_kuliah_id 1
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 12,
+                'ruangan_id' => 2, // Tabrakan dengan mata_kuliah_id 2
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 13,
+                'ruangan_id' => 3, // Tabrakan dengan mata_kuliah_id 3
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 14,
+                'ruangan_id' => 4, // Tabrakan dengan mata_kuliah_id 4
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 15,
+                'ruangan_id' => 5, // Tabrakan dengan mata_kuliah_id 5
+            ],
+        
+            // Jadwal aman lagi
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 16,
+                'ruangan_id' => 6,
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 17,
+                'ruangan_id' => 7,
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 18,
+                'ruangan_id' => 8,
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 19,
+                'ruangan_id' => 9,
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 20,
+                'ruangan_id' => 10,
+            ],
+        
+            // Tabrakan lagi
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 21,
+                'ruangan_id' => 1, // Tabrakan dengan mata_kuliah_id 1 dan 11
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 22,
+                'ruangan_id' => 2, // Tabrakan dengan mata_kuliah_id 2 dan 12
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 23,
+                'ruangan_id' => 3, // Tabrakan dengan mata_kuliah_id 3 dan 13
+            ],
+        
+            // Jadwal aman lagi
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 24,
+                'ruangan_id' => 4,
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 25,
+                'ruangan_id' => 5,
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 26,
+                'ruangan_id' => 6,
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 27,
+                'ruangan_id' => 7,
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 28,
+                'ruangan_id' => 8,
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 29,
+                'ruangan_id' => 9,
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 30,
+                'ruangan_id' => 10,
+            ],
+        
+            // Tabrakan terakhir
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 31,
+                'ruangan_id' => 1, // Tabrakan dengan mata_kuliah_id 1, 11, 21
+            ],
+            [
+                'mahasiswa_id' => 1,
+                'mata_kuliah_id' => 32,
+                'ruangan_id' => 2, // Tabrakan dengan mata_kuliah_id 2, 12, 22
+            ],
         ];
+        
 
         DB::table('irs')->insert($irs);
     }
